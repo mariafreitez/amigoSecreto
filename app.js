13268a53aa1bfc77f18424a81b1821c1aa73fc26
Maria Freitez <mariafreitez2195@gmail.com>
2025-03-09 22:41:01 -0500

    Logica y video

--- app.js
+++ app.js
@@ -1,1 +1,74 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Array para almacenar los nombres de los amigos
+let listaDeAmigos = [];
+
+// Obtener el elemento de la lista visual
+let listaVisual = document.getElementById('listaAmigos');
+
+// Función para agregar amigos a la lista
+function agregarAmigo() {
+    let inputAmigo = document.getElementById('amigo');
+    let nombre = inputAmigo.value.trim();
+    
+    // Verificar si el nombre está vacío
+    if (nombre === '') {
+        alert('Por favor, ingresa un nombre');
+        return;
+    }
+    
+    // Verificar si el nombre ya está en la lista
+    if (listaDeAmigos.find(elemento => elemento.toLowerCase() == nombre.toLowerCase())) {
+        alert('Este nombre ya está en la lista');
+        return;
+    }
+    
+    listaDeAmigos.push(nombre);
+    
+    // Actualizar la lista visual
+    actualizarLista()
+    
+    // Limpiar el input
+    inputAmigo.value = '';
+}
+
+// Función para actualizar la lista visual
+function actualizarLista() {
+    limpiarLista()
+    
+    listaDeAmigos.forEach(nombre => {
+        let nuevoAmigo = document.createElement('li');
+        nuevoAmigo.textContent = nombre;    
+        listaVisual.appendChild(nuevoAmigo);
+    });
+} 
+
+// Función para limpiar la lista visual
+function limpiarLista() {
+    listaVisual.innerHTML = '';
+}   
+
+// Función para sortear amigos secretos
+function sortearAmigo() {
+    // Verificar si hay al menos 2 personas en la lista
+    if (listaDeAmigos.length < 2) {
+        alert('Se necesitan al menos 2 personas para realizar el sorteo');
+        return;
+    }
+
+    limpiarLista()
+    
+    //Genere un indice aleatoria
+    let indiceAleatorio = Math.floor(Math.random() * listaDeAmigos.length);
+    let amigoSecreto = listaDeAmigos[indiceAleatorio];
+    let resultado = `El amigo secreto sorteado es: ${amigoSecreto}`;
+    
+    // Mostrar resultado
+    let listaResultados = document.getElementById('resultado');
+    listaResultados.innerHTML = '';
+
+    // Crear un elemento li para el resultado
+    let elementoResultado = document.createElement('li');
+    elementoResultado.textContent = resultado;
+    
+    // Agregar el elemento li al ul
+    listaResultados.appendChild(elementoResultado);
+}
